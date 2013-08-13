787ab8b8278aba0b0022e6dc38c3170d80418ce6
intuinno <intuinno@gmail.com>
2013-08-12 22:04:43 -0400

    Added static files

--- polls/tests.py
+++ polls/tests.py
@@ -4,13 +4,19 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import datetime
+from django.utils import timezone
+from polls.models import Poll
 
 from django.test import TestCase
 
+class PollMethodTests(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+	def test_was_published_recently_with_future_poll(self):
+		"""
+		was_published_recently() should return False for polls whose
+		pub_date is in the future
+		"""
+		future_poll = Poll(pub_date=timezone.now()+ datetime.timedelta(days=30))
+		self.assertEqual(future_poll.was_published_recently(),False)
+
