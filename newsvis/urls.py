2d07e6b41c2f50ff6394a11a3f491f19e09e3d9c
intuinno <intuinno@gmail.com>
2013-09-18 12:42:49 -0400

    Added news vis

--- newsvis/urls.py
+++ newsvis/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
-from movievis import views
+from newsvis import views
 
 urlpatterns = patterns('',
 
-                       url(r'^save_json$',views.save_comment,name='save_comment'),
-                       url(r'^$', TemplateView.as_view(template_name="movievis/index.html")),
-                       url(r'^(?P<comment_id>\d+)/$', views.detail, name='detail'),
+#                        url(r'^save_json$',views.save_comment,name='save_comment'),
+                       url(r'^$', TemplateView.as_view(template_name="newsvis/index.html")),
+#                        url(r'^(?P<comment_id>\d+)/$', views.detail, name='detail'),
 )
 
