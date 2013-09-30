e20a375f7810b8481d0de24a9ee9e3bacc1f5ed6
intuinno <intuinno@gmail.com>
2013-09-30 18:54:09 -0400

    Added profile for ribbit

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -11,5 +11,8 @@ urlpatterns = patterns('',
                         url(r'^signup$', 'ribbit.views.signup'), # signup
                         url(r'^ribbits$', 'ribbit.views.public'), # public ribbits
                         url(r'^submit$', 'ribbit.views.submit'), # submit new ribbit
+                        url(r'^users/$', 'ribbit.views.users'),
+                        url(r'^users/(?P<username>\w{0,30})/$', 'ribbit.views.users'),
+                        url(r'^follow$', 'ribbit.views.follow'),
 )
 
